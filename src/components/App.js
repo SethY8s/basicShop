2a72630c8746a658fcbbe90cd25d117a70ad2247
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-02-26 15:18:15 +0700

    Sign up styling done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import Signup from "./Signup"
+import { Container } from "react-bootstrap"
 
 function App() {
   return (
+    <Container 
+    classname="d-flex justify-content-center align-items-center"
+    style={{ minHeight: "100vh"}}>
+      <div className="m-auto mt-5"
+      style={{ maxWidth: "400px"}}>
     <Signup />
+    </div>
+    </Container>
   );
 }
 
