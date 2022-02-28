780dabb1f9918406cbb09f3fd580a5acf7f69be1
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-02-28 18:21:21 +0700

    basic route setup

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import Signup from './Signup';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../context/AuthContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './Dashboard'
+import Login from './Login'
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
         <Router>
           <AuthProvider>
             <Routes>
+              <Route exact path="/" element={<Dashboard />} />
               <Route path="/signup" element={<Signup />} />
+              <Route path="login" element ={<Login />} />
             </Routes>
           </AuthProvider>
         </Router>
