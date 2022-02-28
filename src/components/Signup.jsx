eea6fdd89d9cc8b0d2c405efdf38304fc3df85ac
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-02-28 16:16:20 +0700

    working error handling of signup form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,17 +1,30 @@
-import React, { useRef } from 'react';
-import { Form, Button, Card } from 'react-bootstrap';
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../context/AuthContext';
 
 export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
-  const { signup } = useAuth()
+  const { signup } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  function handleSubmit(e) {
-      e.prevent.Default()
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-      signup(emailRef.current.value, passwordRef.current.value)
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Password do not match');
+    }
+
+    try {
+      setError('');
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError('Failed to create an account');
+    }
+    setLoading(false);
   }
 
   return (
@@ -19,6 +32,8 @@ export default function Signup() {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
           <Form.Group id="Email">
             <Form.Label>Email</Form.Label>
             <Form.Control type="email" ref={emailRef} required></Form.Control>
@@ -41,9 +56,10 @@ export default function Signup() {
               required
             ></Form.Control>
           </Form.Group>
-          <Button className="w-100 mt-3" type="submit">
+          <Button disabled={loading} className="w-100 mt-3" type="submit">
             Sign Up
           </Button>
+          </Form>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
