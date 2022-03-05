ff72a441396c8d4fc4d1c65e1064325e6d75ca0f
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-03-05 20:42:52 +0700

    added styling to home page and made mock product

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './Product.module.css'
+import productCSS from './Product.module.css'
 
 
 export default function Product() {
   return (
-    <div>Product</div>
+      
+    
+        <div className={productCSS.productContainer}>
+
+        <img src="https://wallpaperaccess.com/full/7871566.png" alt="ape" />
+
+       
+        
+        <h2 className={productCSS.productText}>Ape</h2>
+        
+        
+        
+        <span className={productCSS.btnContainer}>
+        <button className='btn btn-sm btn-primary m-2'>Add to Cart</button>
+        <button className='btn btn-sm btn-primary m-2'>View Item</button>
+        </span>
+        </div>
+        
+
+    
+    
   )
 }
--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Product from './Product'
 
+
 export default function Products() {
   return (
-    <div>
+    <div className='d-flex justify-content-around mt-5'>
+        <Product />
         <Product />
     </div>
   )
