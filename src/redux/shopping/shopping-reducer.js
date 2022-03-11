d0fc255783426658560287d6488f1e49fb27a6df
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-03-11 12:55:45 +0700

    working shopping-reducer

--- src/redux/shopping/shopping-reducer.js
+++ src/redux/shopping/shopping-reducer.js
@@ -4,30 +4,30 @@ const INITIAL_STATE = {
   products: [
     {
       id: 1,
-      title: "Rubix Cube",
+      title: 'Rubix Cube',
       description:
-        "This cube will keep you busy the entire day and it is very fun to play with",
+        'This cube will keep you busy the entire day and it is very fun to play with',
       price: 15.0,
       image:
-        "https://images.unsplash.com/photo-1591991731833-b4807cf7ef94?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        'https://images.unsplash.com/photo-1591991731833-b4807cf7ef94?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
     },
     {
       id: 2,
-      title: "Large Coffee Cup",
+      title: 'Large Coffee Cup',
       description:
-        "Get a big cup of coffee every morning before the day starts",
+        'Get a big cup of coffee every morning before the day starts',
       price: 20.0,
       image:
-        "https://images.unsplash.com/photo-1572119865084-43c285814d63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        'https://images.unsplash.com/photo-1572119865084-43c285814d63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
     },
     {
       id: 3,
-      title: "10 Books",
+      title: '10 Books',
       description:
-        "These books will keep you busy all throughout the entire lockdown and give you some great advise from famous people",
+        'These books will keep you busy all throughout the entire lockdown and give you some great advise from famous people',
       price: 150.0,
       image:
-        "https://images.unsplash.com/photo-1512820790803-83ca734da794?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1374&q=80",
+        'https://images.unsplash.com/photo-1512820790803-83ca734da794?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1374&q=80',
     },
   ], //(id, title, description, price, img)
   cart: [], //(id, title, description, price, img, qty)
@@ -37,16 +37,47 @@ const INITIAL_STATE = {
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case actionTypes.ADD_TO_CART:
-      return {};
+      // Great Item data from products array
+      const item = state.products.find(
+        (product) => product.id === action.payload.id
+      );
+      // Check if Item is in cart already
+      const inCart = state.cart.find((item) =>
+        item.id === action.payload.id ? true : false
+      );
+
+      return {
+        ...state,
+        cart: inCart
+          ? state.cart.map((item) =>
+              item.id === action.payload.id
+                ? { ...item, qty: item.qty + 1 }
+                : item
+            )
+          : [...state.cart, { ...item, qty: 1 }],
+      };
     case actionTypes.REMOVE_FROM_CART:
-      return {};
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload.id),
+      };
     case actionTypes.ADJUST_QTY:
-      return {};
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, qty: +action.payload.qty }
+            : item
+        ),
+      };
     case actionTypes.LOAD_CURRENT_ITEM:
-      return {};
+      return {
+        ...state,
+        currentItem: action.payload,
+      };
     default:
       return state;
   }
 };
 
-export default shopReducer
+export default shopReducer;
